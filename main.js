5ad818d343ee0e9d25708d7957ddeef32af5ecc4
Anandhbasha <anadhakm@gmail.com>
2025-09-06 15:19:28 +0530

    functions

---
+++ main.js
@@ -0,0 +1,95 @@
+// console.log("Hello");
+
+// hoisting
+// var
+// let
+// const
+// // console.log(a);
+
+// var a = 20
+// var a = 60
+// a=70
+// console.log(a);
+// // console.log("Hello"+a);
+// // let 
+// let b=60
+// b=80
+// console.log(b)
+// // const 
+// const c =50
+// // c=80
+// // console.log(c);
+
+// {
+//     let f=80
+//     console.log(f);
+// }
+// console.log(f);
+
+
+// // datatypes
+//     //premitive datatypes
+//         let a =10
+//         // number
+//         let user = "arun"
+//         // string
+//         let isComing = true
+//         //boolean
+//         let d=undefined;
+//         console.log(d)        
+//         // undefined
+//         // nan - Not an number
+//         // null
+//         let f = null
+//     // non premitive datatypes
+//         // array
+//             let arr1 = [55,true,undefined,"ajay"]
+//             // console.log(arr1);
+//             console.log(arr1[0]);
+//             console.log(arr1[1]);
+//             console.log(arr1[2]);
+//             console.log(arr1[3]);
+
+//         // object
+//         const person = {
+//             names:"abd",
+//             age:20,
+//             City:"CBE",
+//             Address:"5645645gyvhvhvhv",
+//             family:{
+//                 dad:{
+//                     fatherName:"gdfr",
+//                     isAlive:false,
+//                 },
+//                 mom:"hjgh",
+//                 siblings:{
+//                     brother:"thrf",
+//                     sis:"ghfhjbkj"
+//                 }
+//             }
+//         }
+        
+//         // console.log(person);
+//         console.log(person.names);
+//         console.log(person.family);
+//         console.log(person.family.dad);
+//         console.log(person.family.siblings.brother);
+        
+// functions
+// function add(a,b){
+//     return a+b    
+// }
+// add(50,70)
+// add(22,44)
+
+
+// return
+// console.log(add(120,211));
+
+
+// arrow
+const add = (a,b)=>{return a+b}
+
+console.log(add(120,211));
+
+
