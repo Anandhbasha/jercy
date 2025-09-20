db115aa08dedc5e55e805ac89583f5d82603eaa3
Anandhbasha <anadhakm@gmail.com>
2025-09-20 15:18:52 +0530

    loops

--- main.js
+++ main.js
@@ -88,8 +88,120 @@
 
 
 // arrow
-const add = (a,b)=>{return a+b}
+// const add = (a,b)=>{return a+b}
 
-console.log(add(120,211));
+// console.log(add(120,211));
 
+// operators
+    // arithmetic Operators
+    //     // +,-,*,/,%,++,--
+    //     let num1=10
+    //     let num2=3
+    //     console.log(num1%num2);
 
+    //     console.log(num1++);
+    //     console.log(num1);
+    //     console.log(++num1);
+    // // comparison Operators
+    //     // <,>,<=,>=,==,===
+
+    //     console.log(num1<=12);
+    //     console.log(num1>=12);
+    //     let newvalue = 10
+    //     console.log(newvalue=="10");
+    //     console.log(newvalue==="10");
+    // // logical
+    //     // &&,||,!
+    //     console.log(10<15 && 50>55 && num1<13);
+    //     console.log(10>15 || 50>55 && num1<13);
+    //     console.log(num1!=5);
+    // // ternary operator
+    //     let age=20
+    //     console.log(age>=20?"Adult":"Teenager");
+    
+
+    // if else
+    let age=12
+    if(age>=20){
+        console.log("Adult");        
+    }else if(age<=19 && age<=13){
+        console.log("Child");        
+    }
+    else{
+        console.log("Teeanger");        
+    }
+
+    // switch
+    let today = "Saturday"
+    switch(today){
+        case "Monday":
+            console.log("Today is Monday")
+            break            
+        case "Tuesday":
+            console.log("Today is Tuesday")
+            break            
+        case "Wednesday":
+            console.log("Today is Wednesday") 
+            break           
+        case "Thursday":
+            console.log("Today is Thursday") 
+            break           
+        case "Friday":
+            console.log("Today is Friday")
+            break            
+        case "Saturday":
+            console.log("Today is Saturday")  
+            break          
+        default:
+            console.log("Today is Sunday")            
+    }
+    
+
+    // loops
+    // while
+    let number1 = 10
+    // while(number1<15){
+    //     console.log("While is working");
+    //     number1++
+    // }
+    // // do while
+    // do{
+    //     console.log("Dowhile is Working");        
+    // }while(10>15)
+    // let arr1 = [10,50,88,77,55,23]
+    // console.log(arr1[0]);
+    // console.log(arr1[1]);
+    // console.log(arr1[2]);
+    // console.log(arr1[3]);
+    // console.log(arr1[4]);
+    // console.log(arr1[5]);
+    
+    // for
+    let arr1 = [10,50,88,77,55,23]
+    // let total=0
+    //    for(let x=0;x<arr1.length;x++){
+    //         total+=arr1[x]
+    //    } 
+    //    console.log(total);
+       
+    
+        // even number
+    //    for(let x=0;x<arr1.length;x++){
+    //         if(arr1[x]%2!=0){
+    //             console.log(arr1[x]);            
+    //         }
+    //    } 
+    //for of
+    for(let x of arr1){
+        console.log(x);        
+    }
+    // for in
+    for(let x in arr1){
+        console.log(x);        
+    }
+    // for each
+    arr1.forEach((item)=>console.log(item))
+
+        
+        
+        
